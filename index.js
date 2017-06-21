82d9deb03b1886cbe585ae9124685b4502de681f
sasaplus1 <sasaplus1@gmail.com>
2017-06-21 15:36:34 +0900

    fixed for Safari

--- index.js
+++ index.js
@@ -16,9 +16,9 @@
       '<svg xmlns="http://www.w3.org/2000/svg" width="' + canvas.width + '" height="' + canvas.height + '">' +
         '<foreignObject width="100%" height="100%">' +
           '<div xmlns="http://www.w3.org/1999/xhtml" style="font-size:40px; width: 100px; overflow-wrap: break-word; text-align: center; line-height: 1; font-family: serif;">' +
-            input.value.split('').map(
-              (char) => '<div>' + char + '</div>'
-            ).join('') +
+            input.value.split('').map(function(char) {
+              return '<div>' + char + '</div>';
+            }).join('') +
           '</div>' +
         '</foreignObject>' +
       '</svg>'
@@ -27,7 +27,9 @@
     svg = new Blob([
       data
     ], {
-      type: 'image/svg+xml;charset=utf-8'
+      // NOTE: Safari cannot draw text if use below
+      //type: 'image/svg+xml;charset=utf-8'
+      type: 'image/svg+xml'
     });
     url = DOMURL.createObjectURL(svg);
 
