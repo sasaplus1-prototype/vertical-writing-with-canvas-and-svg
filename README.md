58c6eb39fbf4335c11b8686d3679a44a899c4c1a
sasaplus1 <sasaplus1@gmail.com>
2017-06-21 15:16:19 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# vertical-writing-with-canvas-and-svg
+
+vertical writing with HTML5 Canvas and SVG
+
+## See also
+
+- [DOM オブジェクトを Canvas に描画する](https://developer.mozilla.org/ja/docs/Web/HTML/Canvas/Drawing_DOM_objects_into_a_canvas)
+
+## License
+
+The MIT license.
---
+++ index.js
@@ -0,0 +1,44 @@
+(function(){
+
+  'use strict';
+
+  var input = document.getElementById('js-input');
+
+  var canvas = document.getElementById('js-canvas'),
+      context = canvas.getContext('2d');
+
+  var DOMURL = self.URL || self.webkitURL || self;
+
+  input.addEventListener('input', function(event) {
+    var data, svg, url, image;
+
+    data = [
+      '<svg xmlns="http://www.w3.org/2000/svg" width="' + canvas.width + '" height="' + canvas.height + '">' +
+        '<foreignObject width="100%" height="100%">' +
+          '<div xmlns="http://www.w3.org/1999/xhtml" style="font-size:40px; width: 100px; overflow-wrap: break-word; text-align: center; line-height: 1; font-family: serif;">' +
+            input.value.split('').map(
+              (char) => '<div>' + char + '</div>'
+            ).join('') +
+          '</div>' +
+        '</foreignObject>' +
+      '</svg>'
+    ].join('');
+
+    svg = new Blob([
+      data
+    ], {
+      type: 'image/svg+xml;charset=utf-8'
+    });
+    url = DOMURL.createObjectURL(svg);
+
+    image = new Image();
+    image.onload = function() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(image, 0, 0);
+
+      DOMURL.revokeObjectURL(url);
+    };
+    image.src = url;
+  }, false);
+
+}());
